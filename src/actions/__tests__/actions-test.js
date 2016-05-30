e972faa9075bfdea418c6ffa9aea8219230b95ca
Andrew Clark <acdlite@me.com>
2016-05-30 11:53:33 +0200

    Tests

---
+++ src/actions/__tests__/actions-test.js
@@ -0,0 +1,40 @@
+import test from 'ava';
+import isSubset from 'is-subset';
+import { addTodo, setVisibilityFilter, toggleTodo } from '../';
+
+test('addTodo creates actions with text', t => {
+  t.true(isSubset(addTodo('Write action creators'), {
+    type: 'ADD_TODO',
+    text: 'Write action creators',
+  }));
+  t.true(isSubset(addTodo('Write reducers'), {
+    type: 'ADD_TODO',
+    text: 'Write reducers',
+  }));
+  t.true(isSubset(addTodo('Write tests'), {
+    type: 'ADD_TODO',
+    text: 'Write tests',
+  }));
+});
+
+test('addTodo creates actions with UUIDs', t => {
+  const a = addTodo('a');
+  const b = addTodo('b');
+  t.is(typeof a.id, 'string');
+  t.is(typeof b.id, 'string');
+  t.not(a.id, b.id);
+});
+
+test('setVisibilityFilter creates action with filter property', t => {
+  t.deepEqual(setVisibilityFilter('SHOW_ALL'), {
+    type: 'SET_VISIBILITY_FILTER',
+    filter: 'SHOW_ALL',
+  });
+});
+
+test('toggleTodo creates action with id property', t => {
+  t.deepEqual(toggleTodo('123'), {
+    type: 'TOGGLE_TODO',
+    id: '123',
+  });
+});
